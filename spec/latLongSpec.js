206e917458ed1a308beff46b2bcb6a6469a76539
Cynthia (Qingwei) Li <Qingwei.Li@ec.gc.ca>
2016-05-09 10:19:36 -0400

    test(geo): move testLatLong to jasmine unit tests

---
+++ spec/latLongSpec.js
@@ -0,0 +1,24 @@
+/* jshint jasmine: true */
+'use strict';
+const fakeBundle = {};
+const fakeGapi = {};
+const layer = require('../src/layer.js')(fakeBundle, fakeGapi);
+
+describe('verify columns of lat/long are numeric', () => {
+    const arr =
+        [
+            ['person0', 'hello', 0.8484, 45.88295],
+            ['person1', 'salut', 24, 45.88295],
+            ['person2', 'nihao', 28, 45.88295],
+            ['person3', 'josun', 84, 45.88295]
+        ];
+
+    it('should be numeric and return true', () => {
+        expect(layer.validateLatLong(arr, 2, 3)).toBeTruthy();
+    });
+
+    it('should not be numeric and return false', () => {
+        expect(layer.validateLatLong(arr, 1, 3)).toBeFalsy();
+    });
+
+});
